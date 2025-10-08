503e9bd02cf3cbc43428b00a9177658e8b09f591
HUNGNAMHOMEPC\truon <truongnhuhungnam@gmail.com>
2025-10-08 10:15:57 +0700

    Refactor AnimatedTitle component to accept dynamic title and container class props; implement GSAP animations for scroll-triggered effects. Update About section to utilize new AnimatedTitle functionality.

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,8 +1,7 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
-
 import { ScrollTrigger } from "gsap/all"
-import AnimatedTitle from "./ui/AnimatedTitle"
+import AnimatedTitle from "~/components/ui/AnimatedTitle"
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -22,31 +21,35 @@ const About = () => {
     clipAnimation.to(".mask-clip-path", {
       width: "100vw",
       height: "100vh",
-      borderRadius: "0",
-      duration: 1,
-      ease: "power1.inOut",
+      borderRadius: 0,
     })
   })
 
   return (
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-        <h2 className="font-general text-sm uppercase md:text-[10px]">
+        <p className="font-general text-sm uppercase md:text-[10px]">
           Welcome to Zentry
-        </h2>
+        </p>
 
-        <AnimatedTitle />
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
+          containerClass="mt-5 !text-black text-center"
+        />
 
         <div className="about-subtext">
-          <p>The Game of Games begins-your life, now an epic MMORPG</p>
-          <p>Zentry unites every player from countless games and platforms</p>
+          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p className="text-gray-500">
+            Zentry unites every player from countless games and platforms, both
+            digital and physical, into a unified Play Economy
+          </p>
         </div>
       </div>
 
-      <div id="clip" className="h-dvh w-screen">
+      <div className="h-dvh w-screen" id="clip">
         <div className="mask-clip-path about-image">
           <img
-            src="/img/about.webp"
+            src="img/about.webp"
             alt="Background"
             className="absolute left-0 top-0 size-full object-cover"
           />
@@ -55,4 +58,5 @@ const About = () => {
     </div>
   )
 }
+
 export default About
